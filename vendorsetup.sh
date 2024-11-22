5e3475bc1e79ea803063832bb3eea71780608d5f
Toufu <hamm157famm7423@gmail.com>
2024-11-22 15:06:02 +0900

    spes: Adopt to WitAqua

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,11 +4,6 @@ GREEN="\033[0;32m"
 YELLOW="\033[1;33m"
 END="\033[0m"
 
-VENDOR_BRANCH="15.0"
-KERNEL_BRANCH="NaughtySilver"
-HARDWARE_BRANCH="lineage-22.0"
-DEBUG_BRANCH="lineage-22"
-
 check_dir() {
     if [ -d "$1" ]; then
         echo -e "${YELLOW}• $1 already exists. Skipping cloning...${END}"
@@ -31,26 +26,6 @@ if [ -f device/qcom/sepolicy_vndr/legacy-um/qva/vendor/bengal/legacy-ims/hal_rcs
     cp device/qcom/sepolicy_vndr/legacy-um/qva/vendor/bengal/legacy-ims/hal_rcsservice.te device/qcom/sepolicy_vndr/legacy-um/qva/vendor/bengal/ims/hal_rcsservice.te
 else
     echo -e "${YELLOW}• Please check your ROM source; the file for legacy imsrcsd sepolicy does not exist. Skipping this step...${END}"
-fi
-
-if check_dir vendor/xiaomi/spes; then
-    echo -e "${GREEN}Cloning vendor sources from spes-development (branch: ${YELLOW}$VENDOR_BRANCH${GREEN})...${END}"
-    git clone https://github.com/spes-development/vendor_xiaomi_spes -b $VENDOR_BRANCH vendor/xiaomi/spes
-fi
-
-if check_dir kernel/xiaomi/sm6225; then
-    echo -e "${GREEN}Cloning kernel sources from spes-development (branch: ${YELLOW}$KERNEL_BRANCH${GREEN})...${END}"
-    git clone https://github.com/spes-development/kernel_xiaomi_sm6225 --depth=1 -b $KERNEL_BRANCH kernel/xiaomi/sm6225
-fi
-
-if check_dir hardware/xiaomi; then
-    echo -e "${GREEN}Cloning hardware sources from LineageOS (branch: ${YELLOW}$HARDWARE_BRANCH${GREEN})...${END}"
-    git clone https://github.com/LineageOS/android_hardware_xiaomi -b $HARDWARE_BRANCH hardware/xiaomi
-fi
-
-if check_dir hardware/samsung-ext/interfaces; then
-    echo -e "${GREEN}Cloning Debugging-Tools from spes-development (branch: ${YELLOW}$DEBUG_BRANCH${GREEN})...${END}"
-    git clone https://github.com/spes-development/hardware_samsung-extra_interfaces -b $DEBUG_BRANCH hardware/samsung-ext/interfaces
 fi
 
 echo -e "${YELLOW}All patches have been successfully applied; your device sources are now ready!${END}"
